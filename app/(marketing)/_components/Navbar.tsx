c3bc2962126d1d3c435523c35b5e3f820af8224d
aravind1699 <arav232sri@gmail.com>
2023-10-18 14:03:36 +0530

    iconpicker and toolbar added

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
         {isAuthenticated && !isLoading && (
           <>
             <Button variant="ghost" size="sm" asChild>
-              <Link href="/documents">Enter Insightify</Link>
+              <Link href="/document">Enter Insightify</Link>
             </Button>
             <UserButton afterSignOutUrl="/" />
           </>
